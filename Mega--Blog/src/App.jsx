e54781fe5be1346c69acecc46e11a3abc3fe8f48
simran <simransingh956429@gmail.com>
2024-09-11 21:15:38 +0530

    COMMIT

---
+++ Mega--Blog/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  console.log(process.env.REACT_APP_APPWRITE_URL);
+  
+  return (
+  <>
+  <h1>A Blog app with Appwrite </h1>
+  </>
+  )
+}
+
+export default App
